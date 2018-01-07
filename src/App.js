6c11f51597c448125f0ece50241198472b28aece
Erik Mogensen <erik@mogensoft.net>
2018-01-07 14:32:43 +0100

    Implemented most of the app
    - Lifted most of the CSS from davidkpiano's example app
    - Added a few react components, it felt right
    
    The app consists of the search form and the results panel and a zoomed
    in photo.
    
    The events generally bubble up to the top which holds the most of the
    state.
    
    The search action has a long delay to fake cancelability, since the
    fetch APIs don't generally support cancels.
    
    The code _works_ but the behaviour is quite buggy, partly because of
    the bad design, the dependency on booleans and "forgetting" to reset
    them when they should be reset.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,52 @@ import React, { Component } from 'react';
 import './App.css';
 
 import fetchJsonp from 'fetch-jsonp';
+
+/** calls onChange when the text is modified, with the value of the text field
+ *  calls onSubmit when the search form is submitted.
+ */
+class SearchForm extends Component {
+  render() {
+    return (
+        <form onSubmit={e => this.props.onSubmit(e)}>
+          <input type="search" onChange={(e) => this.props.onChange(e.target.value)} placeholder="Search flicker for images!!"/>
+          <div class="buttons">
+            <input type="submit" value="Search!"/>
+            <input type="button" onClick={(e) => this.props.onCancel()} value="Cancel"/>
+          </div>
+        </form>
+    );
+  }
+}
+
+
+class Image extends Component {
+  render() {
+    return (
+        <img
+            alt={this.props.alt}
+            src={this.props.src}
+            onClick={(e) => this.props.onSelect(this.props.src)}
+            style={{'--i': this.props.index}}
+            className={this.props.className}
+        />
+    );
+  }
+}
+
+
+class ResultsPanel extends Component {
+  render() {
+    // TODO: for each image in results, show image with zoom control.
+    const results = this.props.results.map((result, index) => (
+      <Image key={result.media.m} src={result.media.m} onSelect={(e) => this.props.onZoom(result.media.m)} index={index} />
+    ));
+    return <section className="results">
+      {results}
+    </section>;
+  }
+}
+
 /*
  * A business object with a search function that returns a promise of
  * an array of images from flickr.
@@ -20,18 +66,89 @@ class BusinessObject {
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // the business object where the backend chatting happens
+    this.business = new BusinessObject();
+
+    // The state that the UI cares about
+    this.state = {
+      // the "mode" of the UI
+      loading: false,
+
+      // The currently "selected image" if any
+      selectedImage: null,
+
+      // the state in the text field
+      text: "",
+
+      // the results to show to the user
+      results: []
+    };
+
+    this.zoom = null;
+    this.results = null;
+  }
+
   render() {
+    if (this.state.loading) {
+      this.loading = <div class="loading">Loading, please wait</div>;
+    }
+    else if (this.state.selectedImage) {
+      this.zoom = <Image className="zoomed-in" src={this.state.selectedImage} onSelect={e => this.handleUnzoom()}/>;
+    }
+    else if (this.state.results) {
+      this.results = <ResultsPanel onZoom={e => this.handleZoom(e)} results={this.state.results}/>;
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={this.state.mode}>
+        <SearchForm onChange={e => this.setState({...this.state, text: e})}
+                    onSubmit={e => this.handleSearch(e, this.state.text)}
+                    onCancel={e => this.handleCancel(this.state.text)}
+        />
+        {this.state.loading ? this.loading : this.state.selectedImage ? this.zoom : this.results}
       </div>
     );
+  }
+
+  handleSearch(event, what) {
+    event.preventDefault();
+    this.setState({...this.state, loading: true});
+    // Fake a slow server to highlight the "loading" state
+    this.XHR = setTimeout(() => {
+      this.business.search(what)
+        .then(results => {
+          console.log(results);
+          this.setState({
+            ...this.state,
+            loading: false,
+            results: results
+          });
+        });
+    }, Math.random() ** 4 * 3000); // mostly short requests, but intermittent "slowness"
+  }
+
+  handleCancel() {
+    console.log("Canceling search");
+    if (this.XHR) {
+      clearTimeout(this.XHR);
+      this.XHR = null;
+    }
+    this.setState({
+      ...this.state,
+      loading: false
+    });
+  }
+
+  handleZoom(what) {
+    console.log("Zooming image number", what);
+    this.setState({...this.state, selectedImage:  what});
+  }
+
+  handleUnzoom() {
+    console.log("Unzooming");
+    this.setState({...this.state, selectedImage:  ""});
   }
 }
 
